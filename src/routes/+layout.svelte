504b4baa815b5b1f50d02114dfe0fb8ae8cd65c6
Hajime Nagisa <jyoichirou99@gmail.com>
2024-07-28 21:45:57 +0900

    Update +layout.svelte
    root page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,10 @@
 <script>
 	import '../app.css';
 </script>
+
+<div class="flex flex-col justify-center items-center w-full">
+	<div class="h-[146px] w-full"></div>
+	<div class="w-full max-w-[460px] flex flex-col justify-start items-start px-[5px] mx-auto">
+		<slot></slot>
+	</div>
+</div>

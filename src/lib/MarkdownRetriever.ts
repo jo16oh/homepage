b91e01e702d064a8d941c0584cd932f954589f11
Hajime Nagisa <jyoichirou99@gmail.com>
2024-07-30 21:19:07 +0900

    feat: MarkdownRetriever

---
+++ src/lib/MarkdownRetriever.ts
@@ -0,0 +1,84 @@
+import * as fs from 'node:fs';
+import * as path from 'node:path';
+import matter from 'gray-matter';
+
+type MarkdownDocument = {
+	fileName: string;
+	created_at: Date;
+	updated_at: Date;
+	prev: string | null;
+	next: string | null;
+	content: string;
+};
+
+type MarkdownRetriever = {
+	journal: Map<string, MarkdownDocument>;
+	blog: Map<string, MarkdownDocument>;
+};
+
+const journals: MarkdownDocument[] = fs
+	.readdirSync('assets/journal')
+	.map((file) => {
+		const data = fs.readFileSync(path.resolve('assets/journal', file), 'utf8');
+		const doc = matter(data);
+
+		if (!doc.data.created_at || !doc.data.updated_at) throw new Error('timestamp not found!');
+
+		return {
+			fileName: file.slice(0, file.length - 3),
+			created_at: new Date(doc.data.created_at as string),
+			updated_at: new Date(doc.data.updated_at as string),
+			content: doc.content
+		};
+	})
+	.sort((a, b) => a.created_at.getTime() - b.created_at.getTime())
+	.map((e, index, array) => {
+		const prev = array[index - 1]?.fileName || null;
+		const next = array[index + 1]?.fileName || null;
+		return {
+			...e,
+			prev: prev,
+			next: next
+		};
+	});
+
+const blogs: MarkdownDocument[] = fs
+	.readdirSync('assets/blog')
+	.map((file) => {
+		const data = fs.readFileSync(path.resolve('assets/blog', file), 'utf8');
+		const doc = matter(data);
+
+		if (!doc.data.created_at || !doc.data.updated_at) throw new Error('timestamp not found!');
+
+		return {
+			fileName: file.slice(0, file.length - 3),
+			created_at: new Date(doc.data.created_at as string),
+			updated_at: new Date(doc.data.updated_at as string),
+			content: doc.content
+		};
+	})
+	.sort((a, b) => a.created_at.getTime() - b.created_at.getTime())
+	.map((e, index, array) => {
+		const prev = array[index - 1]?.fileName || null;
+		const next = array[index + 1]?.fileName || null;
+		return {
+			...e,
+			prev: prev,
+			next: next
+		};
+	});
+
+const blogMap = new Map<string, MarkdownDocument>();
+for (const i of blogs) {
+	blogMap.set(i.fileName, i);
+}
+
+const journalMap = new Map<string, MarkdownDocument>();
+for (const i of journals) {
+	journalMap.set(i.fileName, i);
+}
+
+export const MarkdownRetriever: MarkdownRetriever = {
+	blog: blogMap,
+	journal: journalMap
+};

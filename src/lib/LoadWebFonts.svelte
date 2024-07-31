5cb452b997cd418053f9a7e2971a0183f4de4afe
Hajime Nagisa <jyoichirou99@gmail.com>
2024-07-31 21:53:08 +0900

    feat: LoadWebFonts.svelte

---
+++ src/lib/LoadWebFonts.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	let { heading, main }: { heading: string; main: string } = $props();
+</script>
+
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link
+		href={'https://fonts.googleapis.com/css2?family=Courier+Prime&family=Sawarabi+Gothic&display=swap' +
+			heading}
+		rel="stylesheet"
+	/>
+	<link
+		href={'https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap' + main}
+		rel="stylesheet"
+	/>
+</svelte:head>

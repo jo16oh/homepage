9e85c9ba87c8292ede6228a22835c98e00fe6844
Hajime Nagisa <jyoichirou99@gmail.com>
2024-08-01 21:11:35 +0900

    feat: Header Component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,12 @@
 <script>
 	import '../app.css';
+	import Header from '$lib/Components/Header.svelte';
 </script>
 
-<div class="flex w-full flex-col items-center justify-center">
-	<div class="h-[146px] w-full"></div>
-	<div class="mx-auto flex w-full max-w-[660px] flex-col items-start justify-start px-[5px]">
+<Header />
+
+<div class="flex w-full flex-col items-center justify-center py-36">
+	<div class="mx-auto flex w-full max-w-[660px] flex-col items-start justify-start px-1">
 		<slot></slot>
 	</div>
-	<div class="h-[146px] w-full"></div>
 </div>

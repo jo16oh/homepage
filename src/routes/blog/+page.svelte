a6de7158de5d734b436ecb969b251c817ba63adf
Joichiro Hayashi <jyoichirou99@gmail.com>
2024-08-06 19:23:23 +0900

    improve head tag

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -7,13 +7,17 @@
   let { data }: { data: { blogs: BlogPost[] } } = $props();
 </script>
 
+<svelte:head>
+  <title>blog | jo16oh.dev</title>
+</svelte:head>
+
 <Fade>
   <h1 class="h-14 p-2 font-heading-bold text-3xl font-bold text-red-50">blog</h1>
 
   <a
     href={'/blog/tag'}
     class="ml-2 flex h-full w-fit p-2 font-heading-bold text-lg leading-5 text-red-50 transition hover:bg-gray-500/40"
-    >→ tags</a
+    >→ all tags</a
   >
 </Fade>
 

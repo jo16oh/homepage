bffe81a4c17c5c2807323495aabfd5fec00a7bb4
Joichiro Hayashi <jyoichirou99@gmail.com>
2024-08-06 13:22:46 +0900

    feat: add transition

--- src/routes/blog/tag/+page.svelte
+++ src/routes/blog/tag/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import Fade from '$lib/Components/Fade.svelte';
   import Tag from '$lib/Components/Tag.svelte';
 
   let { data }: { data: { tags: Map<string, string[]> } } = $props();
@@ -8,12 +9,14 @@
     .sort((a, b) => b[1].length - a[1].length);
 </script>
 
-<h1 class="h-14 p-2 font-heading-bold text-3xl text-white">tags</h1>
+<Fade>
+  <h1 class="h-14 p-2 font-heading-bold text-3xl text-white">tags</h1>
 
-<div class="w-fill flex px-2">
-  <div class="inline-flex flex-row flex-wrap items-start justify-start">
-    {#each tags as tag}
-      <Tag name={tag[0]} number={tag[1].length} />
-    {/each}
+  <div class="w-fill flex px-2">
+    <div class="inline-flex flex-row flex-wrap items-start justify-start">
+      {#each tags as tag}
+        <Tag name={tag[0]} number={tag[1].length} />
+      {/each}
+    </div>
   </div>
-</div>
+</Fade>
--- src/routes/journal/[slug]/+page.svelte
+++ src/routes/journal/[slug]/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import type { MarkdownDocument } from '$lib/MarkdownRetriever';
   import BottomNav from '$lib/Components/BottomNav.svelte';
+  import Fade from '$lib/Components/Fade.svelte';
 
   let { data }: { data: { doc: MarkdownDocument; html: string } } = $props();
 </script>
@@ -9,26 +10,30 @@
   <title>{data.doc.title}</title>
 </svelte:head>
 
-<h1 class="font-heading-bold text-3xl font-bold text-white">
-  {data.doc.fileName}
-</h1>
+  <Fade params={{duration: 100}}>
+    <h1 class="font-heading-bold text-3xl font-bold text-white">
+      {data.doc.fileName}
+    </h1>
+  </Fade>
 
-<div class="mt-3 w-auto px-2">
+<Fade params={{duration: 150}}>
+<div class="mt-3 w-full px-2">
   <ul>
-    {#each Object.entries(data.doc.frontmatter) as obj}
-      <li class="font-heading-light text-xs leading-5 text-white">
-        {obj[0]} ： {obj[1]}
-      </li>
+    {#each Object.entries(data.doc.frontmatter) as obj, i}
+          <li class="font-heading-light text-xs leading-5 text-white">
+            {obj[0]} ： {obj[1]}
+          </li>
     {/each}
   </ul>
 </div>
 
-<div class="h-16 w-auto"></div>
+<div class="h-16 w-full"></div>
 
-<div class="w-auto px-1 sm:px-3">
-  {@html data.html}
-</div>
+    <div class="w-full px-1 sm:px-3">
+      {@html data.html}
+    </div>
 
 <div class="h-32 w-full"></div>
 
-<BottomNav prev={data.doc.prev} next={data.doc.next} />
+    <BottomNav prev={data.doc.prev} next={data.doc.next} />
+</Fade>
